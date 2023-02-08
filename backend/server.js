b047636adb10f28fa570a13363fa5bc4ef9523f9
Avash <avash115@gmail.com>
2023-02-08 18:47:54 +0545

    Changes in the initial startup file

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const sequelize = require('./config/database')
+
+sequelize.authenticate().then(
+    () => {
+        console.log("Connection has been established successfully.")
+    }
+).catch(
+    (err) => {
+        console.log(`Unable to connect to the database ${err}`)
+    }
+)
