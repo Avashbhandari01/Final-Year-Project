f78c9c62bf91c47c9ea3a3beac9641d50ecef0f4
Avashbhandari01 <avash115@gmail.com>
2023-04-03 14:35:23 +0545

    Frontend of notification page has been updated.

--- frontend/src/pages/NotificationPage.js
+++ frontend/src/pages/NotificationPage.js
@@ -1,7 +1,37 @@
-import { Helmet } from 'react-helmet-async';
-import { Container, Typography } from '@mui/material';
+import { Helmet } from "react-helmet-async";
+import {
+  Container,
+  Typography,
+  Card,
+  TextField,
+  TextareaAutosize,
+  Button,
+} from "@mui/material";
+import { useRef, useState } from "react";
 
 export default function NotificationPage() {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [message, setMessage] = useState("");
+
+  const form = useRef();
+
+  const sendSMS = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:5000/api/sms/send-sms", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ phoneNumber, message }),
+      });
+      const result = await response.json();
+      console.log(result);
+      form.current.reset();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
@@ -13,7 +43,33 @@ export default function NotificationPage() {
         <Typography variant="h4" sx={{ mb: 5 }}>
           Notification
         </Typography>
-        <h1>This is a notification page.</h1>
+        <Card style={{ padding: "20px", marginBottom: "30px" }}>
+          <h3>Send SMS</h3>
+          <form ref={form}>
+            <TextField
+              margin="normal"
+              fullWidth
+              label="Enter phone number..."
+              name="phoneNumber"
+              onChange={(event) => setPhoneNumber(event.target.value)}
+            />
+            <TextareaAutosize
+              name="message"
+              aria-label="minimum height"
+              minRows={3}
+              placeholder="Enter a message..."
+              style={{ width: "72.4vw", marginTop: "20px" }}
+              onChange={(event) => setMessage(event.target.value)}
+            />
+          </form>
+          <Button
+            variant="contained"
+            style={{ marginTop: "2vw" }}
+            onClick={sendSMS}
+          >
+            Send
+          </Button>
+        </Card>
       </Container>
     </>
   );
